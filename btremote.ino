123898d0f23a395628e1dfea8eee4c932761b969
Brandon Lewis <dotsony@gmail.com>
2015-04-03 00:35:20 -0700

    start implementing the main screen

--- btremote.ino
+++ btremote.ino
@@ -9,9 +9,24 @@ sketch.
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 #include <AdaEncoder.h>
+#include <string.h>
 #include "WheelUI.h"
 
 #define LEN(x) (sizeof(x) / sizeof(x[0]))
+
+typedef const unsigned char PROGMEM static_icon[];
+
+/*
+ * Characters per line at different font size settings. This assumes
+ * the default orientation.
+ */
+const uint8_t CHARS_PER_LINE[] = {
+   14,
+   14,
+   6,
+   5,
+};
+
 
 /*
  * Contrast Adjustment.
@@ -46,6 +61,85 @@ class ContrastAdjustment : public Screen {
       uint8_t m_knob_id;
       uint8_t m_back_button;
 };
+
+/*
+ * Main screen
+ */
+static const unsigned char PROGMEM SPEAKER_ICON[] = {
+   B00000101, B10000000,
+   B00001100, B01000000,
+   B00011101, B00100000,
+   B11111100, B10100000,
+   B11111100, B10100000,
+   B00011101, B00100000,
+   B00001100, B01000000,
+   B00000101, B10000000,
+};
+
+class MainScreen : public Screen {
+   public:
+      MainScreen() :
+	 m_playing(false),
+	 m_artist_scroll(m_artist),
+	 m_track_scroll(m_track)
+      {
+	 strncpy(m_artist, "Phil Collins", sizeof(m_artist));
+	 strncpy(m_track, "In the air tonight", sizeof(m_track));
+      };
+      
+      void draw(Adafruit_GFX &display) {
+	 uint8_t w = display.width() - 1;
+	 uint8_t h = display.height() - 1;
+	 uint8_t pos = (millis() / 100) % w;
+
+	 display.setTextSize(1);
+	 display.setTextWrap(false);
+	 display.setCursor(-pos, 0);
+	 m_artist_scroll.draw(display);
+	 m_track_scroll.draw(display);
+
+	 if (m_playing) {
+	    display.fillTriangle(
+	       0, h - 10,
+	       5, h - 5,
+	       0, h,
+	       BLACK);
+	 } else {
+	    display.fillRect(
+	       0, h - 10,
+	       3, 10,
+	       BLACK);
+
+	    display.fillRect(
+	       4, h - 10,
+	       3, 10,
+	       BLACK);	       
+	 }
+
+	 display.drawBitmap(
+	    w - 16,
+	    h - 8,
+	    SPEAKER_ICON,
+	    16, 8,
+	    BLACK);
+      };
+
+      void handle_event(UI &ui, Event &event) {
+	 switch (event.source) {
+	    case BUTTON_PRESS:
+	       m_playing = !m_playing;
+	       break;
+	 };
+      };
+
+   private:
+      char m_artist[20];
+      char m_track[20];
+      boolean m_playing;
+      ScrolledText<20,  0, 14> m_artist_scroll;
+      ScrolledText<20, 10, 14> m_track_scroll;
+};
+
 
 // Software SPI (slower updates, more flexible pin options):
 Adafruit_PCD8544 display = Adafruit_PCD8544(
@@ -84,21 +178,23 @@ EncoderSrc<'a', 10, 11, WHEEL> volume;
 /*
  * Configure the screens and the main menus.
  */
-TextScreen t1("Test 1", LEFT_BTN);
-ContrastAdjustment contrast(display, ENC_WHEEL, ENC_BTN);
+MainScreen home;
+ContrastAdjustment contrast(display, WHEEL, ENC_BTN);
 TestScreen t3;
 
+#if 0
 MenuItem main_menu_items[] = {
-   {"Test1", t1},
+   {"Home", home},
    {"Contrast", contrast},
    {"Test3", t3},
 };
 Menu<LEN(main_menu_items)> main_menu(main_menu_items);
+#endif
 
 /*
  * Initialize the UI and the main display.
  */
-UI ui(display, main_menu);
+UI ui(display, home);
 
 void loop() {
    // Poll for events
